8ad63884debe3fa6004a20a24dffa529dbea8e71
Peter Izzo <42940178+peter-izzo@users.noreply.github.com>
2018-12-30 02:22:10 -0500

    added new features
    Added code to add all of a specific users followers.
    
    Added the beginning code to tweet random followed users the date and time.

--- text.py
+++ text.py
@@ -46,3 +46,40 @@ def tweet():
             sleep(5)
 
 tweet()
+
+#follows anyone who follows me
+for follower in tweepy.Cursor(api.followers).items():
+    follower.follow()
+
+#looks up a specific users followers and follows them
+ids = []
+for page in tweepy.Cursor(api.followers_ids, screen_name="any_screen_name").pages():
+    ids.extend(page)
+    sleep(30)
+    #makes userID list into text file
+    with open('user_ids.txt', 'w') as file:
+        for i in ids:
+            file.write("%s\n" % i)
+
+user_list = open("textfile.txt", "r")
+open_user = user_list.readlines()
+user_split = user_list.read().splitlines()
+
+for i in open_user:
+    api.create_friendship(i)
+user_list.close()
+
+#trying to get the screen anmes from user id list
+#this isn't working yet
+def screen_name():
+    for i in open_user:
+        names = list(api.get_user(i))
+        with open('screen_names.txt', 'w') as f:
+            for item in names:
+                f.write("%s\n" % item)
+
+api.followers(screen_name)
+#tweets one random user once a day the current date and datetime
+def send_date():
+    i = random.choice(user_split)
+    api.update_status("@"i " %s")
