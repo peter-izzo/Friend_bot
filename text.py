1af23f9d0e702328d35e055e3b793a8426b89405
Peter Izzo <42940178+peter-izzo@users.noreply.github.com>
2019-01-06 00:49:26 -0500

    Many Updates
    Cleaned up the tweeting from the books. Now only tweets one book at a time and is far less chaotic.
    Actually got the program to create the text doc of added user screen names
    Tweets one random user once per day the current date and time as long as program is running

--- text.py
+++ text.py
@@ -1,6 +1,8 @@
 import tweepy
 from credentials import *
 from time import sleep
+import random
+from datetime import datetime as dt
 
 #accessing OAuth and integrating with API
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -8,16 +10,16 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 #reads lines from one book
-book1 = open("C:\\Users\\Pete\\AppData\\Local\\Programs\\Python\\Python36-32\\Projects\\twitter_bot\\ernest.txt", 'r')
+book1 = open("C:\\your_directory\\ernest.txt", 'r')
 read_it = book1.readlines()
 book1.close()
 
 #reads lines from another book
-book = open("C:\\Users\\Pete\\AppData\\Local\\Programs\\Python\\Python36-32\\Projects\\twitter_bot\\odyssey.txt", 'r')
+book = open("C:\\your_directory\\odyssey.txt", 'r')
 file_lines = book.readlines()
 book.close()
 
-#tweet lines from book1
+#tweet lines from book
 def tweet():
     for i in file_lines:
         try:
@@ -30,22 +32,20 @@ def tweet():
             print(e.reason)
             continue
 
-        sleep(10)
+        sleep(250)
+def tweet2():
+    for i in read_it:
+        try:
+            print(i)
+            if i != '/n':
+                api.update_status(i)
+            else:
+                pass
+        except tweepy.TweepError as e:
+            print(e.reason)
+            continue
+    sleep(100)
 
-        while sleep != False:
-            for i in read_it:
-                try:
-                    print(i)
-                    if i != '/n':
-                        api.update_status(i)
-                    else:
-                        pass
-                except tweepy.TweepError as e:
-                    print(e.reason)
-                    continue
-            sleep(5)
-
-tweet()
 
 #follows anyone who follows me
 for follower in tweepy.Cursor(api.followers).items():
@@ -53,7 +53,7 @@ for follower in tweepy.Cursor(api.followers).items():
 
 #looks up a specific users followers and follows them
 ids = []
-for page in tweepy.Cursor(api.followers_ids, screen_name="any_screen_name").pages():
+for page in tweepy.Cursor(api.followers_ids, screen_name="any_user").pages():
     ids.extend(page)
     sleep(30)
     #makes userID list into text file
@@ -61,7 +61,7 @@ for page in tweepy.Cursor(api.followers_ids, screen_name="any_screen_name").page
         for i in ids:
             file.write("%s\n" % i)
 
-user_list = open("textfile.txt", "r")
+user_list = open('C:\\directory\\user_ids.txt', "r")
 open_user = user_list.readlines()
 user_split = user_list.read().splitlines()
 
@@ -69,17 +69,33 @@ for i in open_user:
     api.create_friendship(i)
 user_list.close()
 
-#trying to get the screen anmes from user id list
-#this isn't working yet
+#adds screen names to text doc
+my_followers = []
 def screen_name():
     for i in open_user:
-        names = list(api.get_user(i))
+        names = api.get_user(i)
+        handels = names.screen_name
+        print(handels)
+        my_followers.append(handels)
         with open('screen_names.txt', 'w') as f:
-            for item in names:
-                f.write("%s\n" % item)
+            for i in my_followers:
+                f.write(i+"\n")
+        #sleep(10)
 
-api.followers(screen_name)
+#unneccessary to do this but possibly needed later on
+sn = open('C:\\screen_names.txt', "r")
+snpick = sn.readlines()
+
 #tweets one random user once a day the current date and datetime
 def send_date():
-    i = random.choice(user_split)
-    api.update_status("@"i " %s")
+    while True:
+        g= datetime.now()
+        i = random.choice(my_followers)
+        api.update_status("@%s" "\n%s" % (i, g))
+        sleep(86400)
+
+#Running functions created above
+tweet1()
+tweet2()
+screen_name()
+send_date()
